8c68ac8e71937ad7205e60d5ac6868e1e31cf176
Abbyrhies <abbyrhies.ronquillo.iics@ust.edu.ph>
2018-09-04 19:24:39 +0800

    RonquilloAbbyrhiesLab1

---
+++ app/src/main/java/com/ronquillo/abbyrhies/Screen1Activity.java
@@ -0,0 +1,13 @@
+package com.ronquillo.abbyrhies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen3);
+    }
+}
